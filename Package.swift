118687f0dfec2373d5026836842dfcf44753c4bb
mertalp <mertalp010@gmail.com>
2024-12-14 00:11:16 +0300

    update package file

--- Package.swift
+++ Package.swift
@@ -1,24 +1,27 @@
-// swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version:5.5
 import PackageDescription
 
 let package = Package(
     name: "BaseMVVMCKit",
+    platforms: [
+        .iOS(.v12) 
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "BaseMVVMCKit",
-            targets: ["BaseMVVMCKit"]),
+            targets: ["BaseMVVMCKit"]
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "BaseMVVMCKit"),
+            name: "BaseMVVMCKit",
+            dependencies: [],
+            path: "Source/BaseKit"
+        ),
         .testTarget(
             name: "BaseMVVMCKitTests",
-            dependencies: ["BaseMVVMCKit"]
+            dependencies: ["BaseMVVMCKit"],
+            path: "Tests/BaseMVVMCKitTests"
         ),
     ]
 )
